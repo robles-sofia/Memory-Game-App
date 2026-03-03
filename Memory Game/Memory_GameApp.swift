be0d78e136026e52edccf52220c7599477cb22e7
Sofia Robles <robles@wustl.edu>
2026-03-02 19:51:42 -0600

    Initial Commit

---
+++ Memory Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory Game
+//
+//  Created by Sofia Robles on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
