46e1bda88a005182ad93cda28fbbfc665335dcb5
Sofia Robles <robles@wustl.edu>
2026-03-03 21:00:27 -0600

    creating the striped pattern for the back of the cards

--- Memory Game/StripedPattern.swift
+++ Memory Game/StripedPattern.swift
@@ -1,1 +1,21 @@
+import SwiftUI
 
+//pattern for the back of cards
+struct StripedPattern: View {
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                let width = geometry.size.width
+                let height = geometry.size.height
+                let spacing: CGFloat = 10
+                
+                for x in stride(from: 0, to: width, by: spacing) {
+                    path.move(to: CGPoint(x: x, y: 0))
+                    path.addLine(to: CGPoint(x: x, y: height))
+                }
+            }
+            
+            .stroke(Color.white.opacity(0.3), lineWidth: 2)
+        }
+    }
+}
