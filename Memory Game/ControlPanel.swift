2a245b4311b8d2108135c6a400d9e1b5119ff58b
Sofia Robles <robles@wustl.edu>
2026-03-03 21:53:40 -0600

    Buttons and text in ControlPanel file

--- Memory Game/ControlPanel.swift
+++ Memory Game/ControlPanel.swift
@@ -1,1 +1,46 @@
+import SwiftUI
 
+struct ControlPanel: View {
+    
+    @ObservedObject var viewModel: CardGameViewModel
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            
+            HStack {
+                Text("Score: \(viewModel.score)")
+                    .font(.headline)
+                
+                Spacer()
+                
+                Text("Moves: \(viewModel.moves)")
+                    .font(.headline)
+            }
+            
+            HStack {
+                Button("New Game") {
+                    withAnimation(.spring()) {
+                        viewModel.startNewGame()
+                    }
+                    
+                }
+                
+                Button("Shuffle") {
+                    withAnimation(.spring()) {
+                        viewModel.shuffleCards()
+                    }
+                }
+            }
+            
+            if viewModel.gameOver {
+                Text("Game Over!")
+                    .font(.title)
+                    .foregroundColor(.green)
+            }
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 5)
+    }
+}
