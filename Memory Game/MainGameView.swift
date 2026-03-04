15776b10187d30a561e203ae22d359b2dfbff45b
Sofia Robles <robles@wustl.edu>
2026-03-03 22:13:03 -0600

    MainGameView file for app layout

--- Memory Game/MainGameView.swift
+++ Memory Game/MainGameView.swift
@@ -1,1 +1,51 @@
+import SwiftUI
 
+struct MainGameView: View {
+    
+    @StateObject private var viewModel = CardGameViewModel()
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Color.blue.opacity(0.2)
+                    .ignoresSafeArea()
+                
+                if horizontalSizeClass == .compact {
+                    VStack {
+                        cardGrid(size: geometry.size)
+                        ControlPanel(viewModel: viewModel)
+                    }
+                } else {
+                    HStack {
+                        cardGrid(size: geometry.size)
+                        ControlPanel(viewModel: viewModel)
+                            .frame(width: 250)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func cardGrid(size: CGSize) -> some View {
+        let columns = [
+            GridItem(.adaptive(minimum: 80))
+        ]
+        
+        return ScrollView {
+            LazyVGrid(columns: columns, spacing: 10) {
+                ForEach(viewModel.cards) { card in
+                    CardView(viewModel: viewModel, card: card)
+                        .aspectRatio(2/3, contentMode: .fit)
+                        .padding(4)
+                }
+            }
+            
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    MainGameView()
+}
